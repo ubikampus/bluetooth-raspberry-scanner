d2d2afe46232b09c35bdd094269ccddbe7ae5428
je-l <je-l@users.noreply.github.com>
2019-07-16 10:27:11 +0300

    Add tests for environment validation

--- pybluez/pybluez.py
+++ pybluez/pybluez.py
@@ -1,17 +1,18 @@
 import json
 import select
+from os import environ
 
 import paho.mqtt.client as mqtt
 import bluetooth
 
-MQTT_URL = "iot.ubikampus.net"
 MQTT_PUB_TOPIC = "ohtu/test2"
 
 
 class MyDiscoverer(bluetooth.DeviceDiscoverer):
-    def __init__(self, mqtt_client):
+    def __init__(self, mqtt_client, raspberry_id):
         super().__init__()
         self.mqtt_client = mqtt_client
+        self.raspberry_id = raspberry_id
 
     def pre_inquiry(self):
         self.done = False
@@ -19,12 +20,12 @@ class MyDiscoverer(bluetooth.DeviceDiscoverer):
     def device_discovered(self, address, device_class, rssi, name):
         message = {
             "beaconId": address,
-            "raspId": 4,
+            "raspId": self.raspberry_id,
             "rssi": rssi,
             "name": name.decode("utf8"),
         }
 
-        self.mqtt_client.publish("ohtu/test2", json.dumps(message))
+        self.mqtt_client.publish(MQTT_PUB_TOPIC, json.dumps(message))
         print("message published to broker: ", message)
 
     def inquiry_complete(self):
@@ -42,17 +43,26 @@ def on_connect(client, userdata, flags, rc):
         raise RuntimeError("failed to connect, return code {}".format(rc))
 
 
+def validate_environ(environ):
+    for key in ("MQTT_URL", "RASPBERRY_ID"):
+        if not environ.get(key):
+            raise RuntimeError(
+                'env var "{}" is missing, see installation.md'.format(key)
+            )
+
+
 def run():
+    validate_environ(environ)
     client = mqtt.Client("P1")
     client.on_connect = on_connect
     client.on_log = on_log
 
-    print("connecting: ", MQTT_URL)
-    client.connect(MQTT_URL)
+    print("connecting: ", environ["MQTT_URL"])
+    client.connect(environ["MQTT_URL"])
     print("connected!")
 
     while True:
-        d = MyDiscoverer(client)
+        d = MyDiscoverer(client, environ["RASPBERRY_ID"])
         d.find_devices(lookup_names=True)
 
         readfiles = [d, ]
